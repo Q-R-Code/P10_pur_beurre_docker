80474228458a9a6eda4337e21f9215187b8ab402
Quentin RUFFIER <qruffier@gmail.com>
2021-04-24 15:49:16 +0200

    add staticfiles

--- pur_beurre_project_app/settings.py
+++ pur_beurre_project_app/settings.py
@@ -129,9 +129,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (str(BASE_DIR.joinpath('pur_beurre_project_app/static')),)
-STATIC_ROOT = (str(BASE_DIR.joinpath('staticfiles')),)
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 CRISPY_TEMPLATE_PACK = "bootstrap4"
 
